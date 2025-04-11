49328613a3c8aea4c81b839ee928e2bfc142a41b
YourGitHubUsername <youremail@example.com>
2025-04-10 21:18:23 -0700

    Enable CORS for frontend clearly

--- src/main.py
+++ src/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from .jd_generator import generate_response
 
-app = FastAPI(title="Skittly API", version="0.1")
+app = FastAPI()
+
+origins = [
+    "https://skittle-frontend-4glr.vercel.app",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class JobDescription(BaseModel):
     job_description: str
